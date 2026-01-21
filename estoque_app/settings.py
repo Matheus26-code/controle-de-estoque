f1f1763cb5974f55bac5e06abc94610c71dd5880
EC2 Default User <ec2-user@ip-172-31-72-6.ec2.internal>
2026-01-21 11:54:12 +0000

    Configuração de produção: RDS PostgreSQL, Nginx e Gunicorn

--- estoque_app/settings.py
+++ estoque_app/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6hs_baar)q4ucyi897+)+9hu3)6kz6uqas^b#k#-m)a)r%^6ju
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['3.237.14.151', 'localhost', '127.0.0.1']
 
 
 # Application definition
